91cf94d283aafc9ece5fd42dbd051a11cbfe1550
Hugo Tamayo <doctamayot@hotmail.com>
2022-08-20 00:55:09 -0500

    image-slide

--- components/ui/ProductSlideShow.tsx
+++ components/ui/ProductSlideShow.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-
+import Image from "next/image";
 import { Zoom } from "react-slideshow-image";
 
 import styles from "./ProductSlideshow.module.scss";
@@ -25,36 +25,47 @@ export const ProductSlideshow: FC<Props> = ({ tecnologies }) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
   }
 
+  // const productImage = useMemo(() => {
+  //   return `/static/images/${tecnologies.images}`;
+  // }, [tecnologies.image]);
+
   return (
     <Box
       sx={{
         width: { sm: "600px", xs: 400, md: "400px", lg: "480px" },
 
-        margin: "00px auto",
+        margin: "0px auto",
       }}
     >
       <Zoom {...zoomOutProperties}>
         {tecnologies.map((i, index) => (
           <div className={styles["each-slide"]} key={index}>
-            <div
+            {/* <div
               style={{
                 backgroundImage: `url(/static/images/${i.images}`,
                 backgroundSize: "contain",
               }}
+            > */}
+
+            <Image
+              src={`/static/images/${i.images}`}
+              alt="Hugo tamayo"
+              width={500}
+              height={250}
+            />
+            <Typography
+              sx={{
+                marginTop: { xs: "0px", sm: "0px" },
+                backgroundColor: "#fff",
+                padding: "5px",
+                fontFamily: "Poppins",
+                color: "#000",
+              }}
             >
-              <Typography
-                sx={{
-                  marginTop: { xs: "160px", sm: "200px" },
-                  backgroundColor: "#fff",
-                  padding: "5px",
-                  fontFamily: "Poppins",
-                  color: "#000",
-                }}
-              >
-                {capitalizarPrimeraLetra(i.clase)}
-              </Typography>
-            </div>
+              {capitalizarPrimeraLetra(i.clase)}
+            </Typography>
           </div>
+          // </div>
         ))}
       </Zoom>
     </Box>
